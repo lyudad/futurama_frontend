32747c98e7146e2ed4cfbc045f4d841058091c7c
Artur Kutusenko <kutusenko@gmail.com>
2022-05-06 00:28:07 +0300

    adds process.env variable

--- src/store/api/passwordResetApi.ts
+++ src/store/api/passwordResetApi.ts
@@ -3,19 +3,21 @@ import { constants } from 'constants/urls';
 
 export const passwordResetApi = createApi({
     reducerPath: 'passwordReset',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_RESET_PASSWORD_BC_URL,
+    }),
     endpoints: (builder) => ({
         passwordReset: builder.mutation({
             query: (body: { password: string; email: string | null }) => ({
                 url: `${constants.PASSWORD_RESET}?email=${body.email}`,
-                method: 'post',
+                method: 'POST',
                 body,
             }),
         }),
         sendEmail: builder.mutation({
             query: (body: object) => ({
                 url: constants.SEND_EMAIL,
-                method: 'post',
+                method: 'PUT',
                 body,
             }),
         }),
