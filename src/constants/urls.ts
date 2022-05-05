e8c605c5f50c5e9b91e2b1a5322b47e9014b5edc
Artur Kutusenko <kutusenko@gmail.com>
2022-05-05 15:13:37 +0300

    Update urls.ts

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,6 @@
 export const constants = {
     USER_LOGIN: '/user/login',
     USER_LOGIN_WITH_GOOGLE: '/user/auth/google/callback',
+    SEND_EMAIL: '/mail/send',
+    PASSWORD_RESET: '/password/reset',
 };
