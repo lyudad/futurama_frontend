31a167b2fa892ae8117b43ae57176857e099f05d
Artur Kutusenko <kutusenko@gmail.com>
2022-05-06 17:58:09 +0300

    adds constants

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -4,7 +4,7 @@ export const constants = {
     SIGNUP: '/signup',
     SIGNUP_ROLE: '/signup/role',
     USER_LOGIN: '/user/login',
-    USER_LOGIN_WITH_GOOGLE: '/user/auth/google/callback,
+    USER_LOGIN_WITH_GOOGLE: '/user/auth/google/callback',
     SEND_EMAIL: '/mail/send',
     PASSWORD_RESET: '/password/reset',
     PASSWORD_NOTOFICATION: '/password/notification',
