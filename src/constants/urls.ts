1258c9d90ecf9d252f1ca2dcf58e84d5afd18fef
Artur Kutusenko <kutusenko@gmail.com>
2022-05-26 01:30:32 +0300

    connected BC/FE

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -10,7 +10,7 @@ export const constants = {
     PASSWORD_MAKE_NEW: '/password/make_new',
     PASSWORD_RECOVERY: '/password/recovery',
     VACANCIES: '/vacancies',
-    GET_ALL_VACANCIES: 'search-work/vacancies/',
+    GET_VACANCIES: 'search_work/vacancies/',
     GET_PROFILE: '/profile',
     USER_CONTACTS: '/user/contacts',
     PHOTO_UPLOAD: 'https://futurama.cf:3011/upload/',
