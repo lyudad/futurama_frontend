cdf35a5bf0c5d5e41a0c06e8082be56146c7f57f
Artur Kutusenko <kutusenko@gmail.com>
2022-05-31 19:57:30 +0300

    Update urls.ts

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -10,6 +10,7 @@ export const constants = {
     PASSWORD_RECOVERY: '/password/recovery',
     VACANCIES: '/vacancies',
     GET_VACANCIES: 'search_work/vacancies/',
+    GET_CATEGORIES: 'search_work/categories/',
     GET_PROFILE: '/profile',
     USER_CONTACTS: '/user/contacts',
     PHOTO_UPLOAD: 'https://futurama.cf:3011/upload/',
